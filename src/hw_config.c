460a90f95f8ec239478d4c6056569269470a2a58
pjaychen <n26021539@mail.ncku.edu.tw>
2013-12-05 19:49:47 +0800

    Let it support USART hardware function was setted in the hw_config.c likes the GPIO, Timers, motion sensor, and USART..

--- src/hw_config.c
+++ src/hw_config.c
@@ -37,10 +37,15 @@ void prvSetupHardware( void )
 	STM_EVAL_PBInit(BUTTON_USER,BUTTON_MODE_EXTI);
 
 	/* Configuration of Timer4 to control LEDs based on MEMS data */
-	prvTIM4_Config();
+	//prvTIM4_Config();
 
 	/* Configure LIS302 in order to produce data used for TIM4 reconfiguration and LED control */
 	prvMEMS_Config();
+
+  RCC_Configuration();
+  GPIO_Configuration();
+  TIM_Configuration();
+  USART_Configuration();
 }
 
 void prvLED_Config(char state)
@@ -76,6 +81,130 @@ void prvLED_Config(char state)
   }
 }
 
+
+/**
+  * @brief  Configures the different system clocks.
+  * @param  None
+  * @retval None
+  */
+void RCC_Configuration(void)
+{
+  
+  RCC_AHB1PeriphClockCmd(  RCC_AHB1Periph_GPIOD , ENABLE );
+  RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM4, ENABLE );
+
+  //Enable GPIO Clocks For USART2
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  //Enable Clocks for USART2
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);    
+}
+
+/**
+  * @brief  configure the PD12~15 to Timers
+  * @param  None
+  * @retval None
+  */
+void GPIO_Configuration(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  //GPIO Configuration for TIM4
+  //   GPIO_StructInit(&GPIO_InitStructure); // Reset init structure
+ 
+  //   GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+  //   GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+  //   GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+  //   GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
+      
+
+  //   // Setup Blue & Green LED on STM32-Discovery Board to use PWM.
+  //   GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15; 
+  // //PD12->LED3 PD13->LED4 PD14->LED5 PD15->LED6
+  // //GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;            // Alt Function - Push Pull
+  // GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF; 
+  //   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  //   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  //   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  //   GPIO_Init( GPIOD, &GPIO_InitStructure );  
+
+    /*----------------------------------------------------------------------*/
+
+  //GPIO Configuration for USART - PA2, PA3
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  //Connect USART pins to AF
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
+
+}
+
+/**
+  * @brief  configure the TIM4 for PWM mode
+  * @param  None
+  * @retval None
+  */
+void TIM_Configuration(void)
+{
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_OCInitTypeDef TIM_OCInitStruct;
+
+    // Let PWM frequency equal 100Hz.
+    // Let period equal 1000. Therefore, timer runs from zero to 1000. Gives 0.1Hz resolution.
+    // Solving for prescaler gives 240.
+    TIM_TimeBaseStructInit( &TIM_TimeBaseInitStruct );
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV4;
+    TIM_TimeBaseInitStruct.TIM_Period = 3360 - 1;   
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 500 - 1; 
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;    
+    TIM_TimeBaseInit( TIM4, &TIM_TimeBaseInitStruct );
+    
+    TIM_OCStructInit( &TIM_OCInitStruct );
+    TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
+    
+    // Initial duty cycle equals 0%. Value can range from zero to 65535.
+    //TIM_Pulse = TIM4_CCR1 register (16 bits)
+    TIM_OCInitStruct.TIM_Pulse = 0; //(0=Always Off, 65535=Always On)
+ 
+    TIM_OC1Init( TIM4, &TIM_OCInitStruct ); // Channel 1  LED
+    TIM_OC2Init( TIM4, &TIM_OCInitStruct ); // Channel 2  LED
+    TIM_OC3Init( TIM4, &TIM_OCInitStruct ); // Channel 3  LED
+    TIM_OC4Init( TIM4, &TIM_OCInitStruct ); // Channel 4  LED
+ 
+    TIM_Cmd( TIM4, ENABLE );
+}
+
+/**
+  * @brief  configure the USART
+  * @param  None
+  * @retval None
+  */
+void USART_Configuration(void)
+{
+  //Structure With Data For USART Configuration
+  USART_InitTypeDef USART_InitStructure;
+  
+  //USART Parameters
+  USART_InitStructure.USART_BaudRate = 19200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No ;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;  
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx ;
+
+  //Configuring And Enabling USART2
+  USART_Init(USART2, &USART_InitStructure);
+  USART_Cmd(USART2, ENABLE);
+
+
+}
+
+/*------------below is original code------------*/
 
 void prvMEMS_Config(void)
 {
--- src/main.c
+++ src/main.c
@@ -22,7 +22,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx.h"
 
-
 /* FreeRTOS includes */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -32,7 +31,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "hw_config.h"
+#include "hw_config.h"  //all hardware configuration was setted here
 #include "main.h"
 
 /** @addtogroup STM32F4-Discovery_Demo
@@ -44,93 +43,56 @@
 #define DELAY 125     /* msec */
 #define queueSIZE	6
 
-/* angle */
-#define G 2
-/* Private macro -------------------------------------------------------------*/
-/*********************************************************************************************************/
-
-
+/*brushless motor PWM max and min duty cycle*/
 #define PWM_MOTOR_MIN 100
 #define PWM_MOTOR_MAX 1000
 #define TEST 200
 
-#define PWM_Motor1 TIM4->CCR1   // 無刷 PWM
-#define PWM_Motor2 TIM4->CCR2   // 無刷 PWM
-#define PWM_Motor3 TIM4->CCR3   // 無刷 PWM
-#define PWM_Motor4 TIM4->CCR4   // 無刷 PWM
+/* angle */
+#define G 2
 
 /* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
+/*PWM signal to drive brushless motor*/
+#define PWM_Motor1 TIM4->CCR1   
+#define PWM_Motor2 TIM4->CCR2   
+#define PWM_Motor3 TIM4->CCR3   
+#define PWM_Motor4 TIM4->CCR4   
 
+/* Private variables ---------------------------------------------------------*/
 
 /* Private function prototypes -----------------------------------------------*/
-
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-
-
 
 /* Private functions ---------------------------------------------------------*/
 
 void pwm(void)
 {
-  volatile int i;
-  int n = 1;
-  uint16_t brightness = 0;      
-  uint16_t who_run = 1;
+  const portTickType xDelay = 1000; // portTICK_RATE_MS;
 
  //Delay_1ms(50);
 
   Motor_Control(PWM_MOTOR_MAX, PWM_MOTOR_MAX, PWM_MOTOR_MAX, PWM_MOTOR_MAX);
 
- Delay_1ms(100);
+  Delay_1ms(100);
 
   Motor_Control(PWM_MOTOR_MIN, PWM_MOTOR_MIN, PWM_MOTOR_MIN, PWM_MOTOR_MIN);
   
- Delay_1ms(100);
+  Delay_1ms(100);
 
   while(1)  // Do not exit
   {
    Motor_Control(TEST, TEST, TEST, TEST);
    
-   Delay_1ms(100);
+   vTaskDelay( xDelay );
 
    Motor_Control(TEST+50, TEST+50, TEST+50, TEST+50);
 
-   Delay_1ms(100);
-
-
-
-    //Light LEDs in turn
-    // switch(who_run){
-    //     case 0:
-    //         TIM4->CCR1 = brightness - 1; // set brightness
-    //         break;
-    //     case 1:
-    //         TIM4->CCR2 = brightness - 1; // set brightness
-    //         break;
-    //     case 2:
-    //         TIM4->CCR3 = brightness - 1; // set brightness
-    //         break;
-    //     case 3:
-    //         TIM4->CCR4 = brightness - 1; // set brightness
-    //         break;
-    // }
+   vTaskDelay( xDelay );
 
   }
  
   return(0); // System will implode
 } 
 
-
-void Delay_1ms( vu32 nCnt_1ms )
-{
-    u32 nCnt;
-	  for(; nCnt_1ms != 0; nCnt_1ms--)
-		    for(nCnt = 56580; nCnt != 0; nCnt--);
-}
-  
 void Motor_Control(u16 Motor1, u16 Motor2, u16 Motor3, u16 Motor4)
 {
 	if(Motor1>PWM_MOTOR_MAX)      Motor1 = PWM_MOTOR_MAX;
@@ -151,79 +113,7 @@ void Motor_Control(u16 Motor1, u16 Motor2, u16 Motor3, u16 Motor4)
 	PWM_Motor4 = Motor4;
 }
 
-/**
-  * @brief  Configures the different system clocks.
-  * @param  None
-  * @retval None
-  */
-void RCC_Configuration(void)
-{
-   RCC_AHB1PeriphClockCmd(  RCC_AHB1Periph_GPIOD , ENABLE );
-   RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM4, ENABLE );
-}
 
-/**
-  * @brief  configure the PD12~15 to Timers
-  * @param  None
-  * @retval None
-  */
-void GPIO_Configuration(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_StructInit(&GPIO_InitStructure); // Reset init structure
- 
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
-    GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
-      
-
-    // Setup Blue & Green LED on STM32-Discovery Board to use PWM.
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15; 
-	//PD12->LED3 PD13->LED4 PD14->LED5 PD15->LED6
-	//GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;            // Alt Function - Push Pull
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF; 
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init( GPIOD, &GPIO_InitStructure );  
-}
-
-/**
-  * @brief  configure the TIM4 for PWM mode
-  * @param  None
-  * @retval None
-  */
-void TIM_Configuration(void)
-{
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
-    TIM_OCInitTypeDef TIM_OCInitStruct;
-
-    // Let PWM frequency equal 100Hz.
-    // Let period equal 1000. Therefore, timer runs from zero to 1000. Gives 0.1Hz resolution.
-    // Solving for prescaler gives 240.
-    TIM_TimeBaseStructInit( &TIM_TimeBaseInitStruct );
-    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV4;
-    TIM_TimeBaseInitStruct.TIM_Period = 3360 - 1;   
-    TIM_TimeBaseInitStruct.TIM_Prescaler = 500 - 1; 
-    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;    
-    TIM_TimeBaseInit( TIM4, &TIM_TimeBaseInitStruct );
-    
-    TIM_OCStructInit( &TIM_OCInitStruct );
-    TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
-    
-    // Initial duty cycle equals 0%. Value can range from zero to 65535.
-    //TIM_Pulse = TIM4_CCR1 register (16 bits)
-    TIM_OCInitStruct.TIM_Pulse = 0; //(0=Always Off, 65535=Always On)
- 
-    TIM_OC1Init( TIM4, &TIM_OCInitStruct ); // Channel 1  LED
-    TIM_OC2Init( TIM4, &TIM_OCInitStruct ); // Channel 2  LED
-    TIM_OC3Init( TIM4, &TIM_OCInitStruct ); // Channel 3  LED
-    TIM_OC4Init( TIM4, &TIM_OCInitStruct ); // Channel 4  LED
- 
-    TIM_Cmd( TIM4, ENABLE );
-}
 /*********************************************************************************************************/
 /* Private functions ---------------------------------------------------------*/
 
@@ -232,6 +122,8 @@ static void vLEDTask( void *pvParameters );
 static void vSWITCHTask( void *pvParameters );
 static void vMEMSTask(void *pvParameters);
 static void vBALANCETask(void *pvParameters);
+static void UsartTask(void *pvParameters);
+
 
 /* handlers to tasks to better control them */
 xTaskHandle xLED_Tasks[4];
@@ -257,7 +149,7 @@ xQueueHandle xQueue;
 int main(void)
 { 
 	/* create a pipe for MEMS->TIM4 data exchange */
-	xQueue=xQueueCreate(1,queueSIZE*sizeof(uint8_t));
+	xQueue = xQueueCreate(1,queueSIZE*sizeof(uint8_t));
 
 	/* create semaphores... */
 	vSemaphoreCreateBinary( xSemaphoreSW );
@@ -275,6 +167,7 @@ int main(void)
 	//xTaskCreate( vLEDTask, ( signed portCHAR * ) "LED6", configMINIMAL_STACK_SIZE, (void *)LEDS[3],tskIDLE_PRIORITY, &xLED_Tasks[3] );
 	//xTaskCreate( vSWITCHTask, ( signed portCHAR * ) "SWITCH", configMINIMAL_STACK_SIZE, NULL,tskIDLE_PRIORITY, NULL );
 	xTaskCreate( vMEMSTask, ( signed portCHAR * ) "MEMS", configMINIMAL_STACK_SIZE, NULL,tskIDLE_PRIORITY, &xMEMS_Task );
+	xTaskCreate( UsartTask, ( signed portCHAR * ) "USART", configMINIMAL_STACK_SIZE, NULL,tskIDLE_PRIORITY, NULL);
 	//xTaskCreate( vBALANCETask, ( signed portCHAR * ) "BALANCE", configMINIMAL_STACK_SIZE, NULL,tskIDLE_PRIORITY, &xBALANCE_Task );
 
 	/* Start the scheduler. */
@@ -284,7 +177,38 @@ int main(void)
 	return 0;  
 }
 
-/*-----------------------------------------------------------*/
+
+
+/* Task functions ------------------------------------------------- */
+
+//Task For Sending Data Via USART
+static void UsartTask(void *pvParameters)
+{
+	while(1) {
+
+		//Variable to store received data	
+		uint32_t Data;
+		
+		//Wait for character
+		while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET) {
+            if (USART_GetFlagStatus(USART2, (USART_FLAG_ORE | USART_FLAG_NE | USART_FLAG_FE | USART_FLAG_PE)))
+				USART_ReceiveData(USART2); // Clear Error
+		}
+
+		//Collect the caracter
+		Data = USART_ReceiveData(USART2);
+
+		//Wait till the flag resets
+		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+
+		//Send the data
+		USART_SendData(USART2, Data); // Echo Char
+	}
+
+	while(1);
+}
+
+
 
 void vMEMSTask(void *pvParameters)
 {
@@ -307,8 +231,6 @@ void vMEMSTask(void *pvParameters)
   	uint8_t TempAcceleration = 0;   
 
 	/* reset offset */
-
-
   	LIS302DL_Read(Buffer_x, LIS302DL_OUT_X_ADDR, 1);
 	LIS302DL_Read(Buffer_y, LIS302DL_OUT_Y_ADDR, 1);
             
@@ -316,117 +238,115 @@ void vMEMSTask(void *pvParameters)
   	YOffset = Buffer_y[0];
 	/* reset */
 
-
-
-for( ;; )
-{
-	counter++;
-	if (counter == 10)
+	for( ;; )
 	{
+		counter++;
+		if (counter == 10)
+		{
 
-  	LIS302DL_Read(Buffer_x, LIS302DL_OUT_X_ADDR, 1);
-	LIS302DL_Read(Buffer_y, LIS302DL_OUT_Y_ADDR, 1);
+	  	LIS302DL_Read(Buffer_x, LIS302DL_OUT_X_ADDR, 1);
+		LIS302DL_Read(Buffer_y, LIS302DL_OUT_Y_ADDR, 1);
 
-      /* Remove the offsets values from data */
-      Buffer_x[0] -= XOffset;
-      Buffer_y[0] -= YOffset;
+	      /* Remove the offsets values from data */
+	      Buffer_x[0] -= XOffset;
+	      Buffer_y[0] -= YOffset;
 
-      /* Update autoreload and capture compare registers value*/
-      temp1 = ABS((int8_t)(Buffer_x[0]));
-      temp2 = ABS((int8_t)(Buffer_y[0]));
-      TempAcceleration = MAX(temp1, temp2);
-	
-	if(TempAcceleration != 0)
-      {
-	
-        if ((int8_t)Buffer_x[0] < -G)
-        {
+	      /* Update autoreload and capture compare registers value*/
+	      temp1 = ABS((int8_t)(Buffer_x[0]));
+	      temp2 = ABS((int8_t)(Buffer_y[0]));
+	      TempAcceleration = MAX(temp1, temp2);
+		
+		if(TempAcceleration != 0)
+	      {
+		
+	        if ((int8_t)Buffer_x[0] < -G)
+	        {
 				STM_EVAL_LEDOn(LED4);
 
 
-                if ((int8_t)Buffer_x[0] <= G)
-                {
-                        STM_EVAL_LEDOff(LED3);
+	            if ((int8_t)Buffer_x[0] <= G)
+	            {
+	                STM_EVAL_LEDOff(LED3);
 
-                }
+	            }
 
-                if ((int8_t)Buffer_y[0] <= G)
-                {
-                       STM_EVAL_LEDOff(LED6);
-                }
+	            if ((int8_t)Buffer_y[0] <= G)
+	            {
+	               STM_EVAL_LEDOff(LED6);
+	            }
 
-                if ((int8_t)Buffer_y[0] >= -G)
-                {
-                        STM_EVAL_LEDOff(LED5);
-                }
+	            if ((int8_t)Buffer_y[0] >= -G)
+	            {
+	                STM_EVAL_LEDOff(LED5);
+	            }
 
-        }
-        if ((int8_t)Buffer_x[0] > G)
-        {
+	        }
+	        if ((int8_t)Buffer_x[0] > G)
+	        {
 				STM_EVAL_LEDOn(LED5);
 
-                                if ((int8_t)Buffer_y[0] <= G)
-                                {
-                                STM_EVAL_LEDOff(LED4);
-                                }
+	            if ((int8_t)Buffer_y[0] <= G)
+	            {
+		            STM_EVAL_LEDOff(LED4);
+	            }
 
-                                if ((int8_t)Buffer_y[0] >= -G)
-                                {
-                        		STM_EVAL_LEDOff(LED3);
-                                }
+	            if ((int8_t)Buffer_y[0] >= -G)
+	            {
+	    			STM_EVAL_LEDOff(LED3);
+	            }
 
-                                if ((int8_t)Buffer_x[0] >= -G)
-                                {
-		                        STM_EVAL_LEDOff(LED6);
-                                }
+	            if ((int8_t)Buffer_x[0] >= -G)
+	            {
+	        	    STM_EVAL_LEDOff(LED6);
+	            }
 
-        }
-        if ((int8_t)Buffer_y[0] > G)
-        {
+	        }
+	        if ((int8_t)Buffer_y[0] > G)
+	        {
 
 				STM_EVAL_LEDOn(LED3);
 
-                                if ((int8_t)Buffer_x[0] <= G)
-                                {
-                                        STM_EVAL_LEDOff(LED4);
-                                }
+	            if ((int8_t)Buffer_x[0] <= G)
+	            {
+	                STM_EVAL_LEDOff(LED4);
+	            }
 
-                                if ((int8_t)Buffer_y[0] >= -G)
-                                {
-                                        STM_EVAL_LEDOff(LED5);
-                                }
+	            if ((int8_t)Buffer_y[0] >= -G)
+	            {
+	                STM_EVAL_LEDOff(LED5);
+	            }
 
-                                if ((int8_t)Buffer_x[0] >= -G)
-                                {
-                                        STM_EVAL_LEDOff(LED6);
-                                }
+	            if ((int8_t)Buffer_x[0] >= -G)
+	            {
+	                STM_EVAL_LEDOff(LED6);
+	            }
 
-        }
-        if ((int8_t)Buffer_y[0] < -G)
-        {
+	        }
+	        if ((int8_t)Buffer_y[0] < -G)
+	        {
 
-			STM_EVAL_LEDOn(LED6);
+				STM_EVAL_LEDOn(LED6);
 
-                                if ((int8_t)Buffer_x[0] <= G)
-                                {
-                                        STM_EVAL_LEDOff(LED3);
-                                }
+	            if ((int8_t)Buffer_x[0] <= G)
+	            {
+	                STM_EVAL_LEDOff(LED3);
+	            }
 
-                                if ((int8_t)Buffer_y[0] <= G)
-                                {
-                               STM_EVAL_LEDOff(LED4);
-                                }
+	            if ((int8_t)Buffer_y[0] <= G)
+	            {
+		           STM_EVAL_LEDOff(LED4);
+	            }
 
-                                if ((int8_t)Buffer_x[0] >= -G)
-                                {
-                                STM_EVAL_LEDOff(LED5);
-                                }
-        }
-		counter = 0x00;
+	            if ((int8_t)Buffer_x[0] >= -G)
+	            {
+	    	        STM_EVAL_LEDOff(LED5);
+	            }
+	        }
+			counter = 0x00;
 
-    }
-  }
-}
+	    }
+	  }
+	}
 }
 
 /*-----------------------------------------------------------*/
